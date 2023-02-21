7a63b52dbc929bed8fe0a16de9995ef83033ff46
Ahmed Shariff <shariff.mfa@outlook.com>
2023-02-21 11:35:44 -0800

    Adding mqtt client& sending data to a broker

--- src/TouchPad.jsx
+++ src/TouchPad.jsx
@@ -1,11 +1,50 @@
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
+import mqtt from "precompiled-mqtt";
 
 class TouchMessage {
-    constructor(threshold)
+    constructor(threshold, messageCallback)
     {
         this.threshold = threshold;
         this.serverAddress = "127.0.0.1";
-        this.port = "80";
+        this.port = 80;
+        this.messagCallback = messageCallback;
+        this.mqttClient = null;
+    }
+
+    connect()
+    {
+        const us = {
+            clientId: Math.random().toString(16),
+            /* username: mqtt.username, */
+            /* password: mqtt.password, */
+            useSSL: false,
+            /* onSuccess: onAction, */
+            /* onFailure: onAction, */
+            protocolId: 'MQTT',
+            protocolVersion: 3,
+            rejectUnauthorized: false,
+            clean: true,
+            /* reconnectPeriod: 20000, */
+            /* connectTimeout: 30 * 1000, */
+            protocol: 'mqtt',
+        };
+
+        // If client exists, make sure it's disconnected
+        if (this.mqttClient !== null)
+        {
+            this.mqttClient.end(true);
+        }
+
+        this.mqttClient = mqtt.connect(`mqtt://${this.serverAddress}:${this.port}`, us);
+
+        // publish messages to UI
+        ["connect", "end", "error", "close", "reconnect", "disconnect", "message"].forEach((val) => {
+            this.mqttClient.on(val, () => {
+                this.messagCallback(val);
+            });
+        });
+
+        this.mqttClient.publish("getReal3D/wandButtonDown", "True");
     }
 
     newTouch(startx, starty)
@@ -33,8 +72,15 @@ class TouchMessage {
 
         if (sendMessage)
         {
+            let pitch = (this.lasty - this.starty) / 100;
+            let yaw = (this.lastx - this.startx) / 100;
             // mqtt send with lastx and lasty
-            console.log(`${this.serverAddress} ${this.port} ${this.lastx - this.startx}  ${this.lasty - this.starty}`)
+            console.log(`${this.serverAddress} ${this.port}   ${yaw}  ${pitch}`)
+            if (this.mqttClient.connected)
+            {
+                this.mqttClient.publish("getReal3D/yawAxis", yaw.toString(5));
+                this.mqttClient.publish("getReal3D/pitchAxis", pitch.toString(5));
+            }
         }
     }
 
@@ -56,7 +102,8 @@ class TouchMessage {
 
 function TouchPad()
 {
-    const [touchMessage] = useState(new TouchMessage(50));
+    const [message, setMessage] = useState("");
+    const [touchMessage] = useState(new TouchMessage(5, setMessage));
     const [textFeedback, setTextFeedback] = useState("Touch");
     const [workingAddress, setWorkingAddress] = useState(touchMessage.getFullAddress());
     const [started, setStarted] = useState(false);
@@ -74,6 +121,11 @@ function TouchPad()
         let val = prompt("Address", "127.0.0.1");
         touchMessage.setAddress(val);
         setWorkingAddress(touchMessage.getFullAddress());
+    }
+
+    function connect()
+    {
+        touchMessage.connect();
     }
 
     function getClientXY (e)
@@ -128,10 +180,12 @@ function TouchPad()
     return (
         <div className="w-full">
             <p className="center">{textFeedback}</p>
+            <p className="center">Connection state: {message}</p>
             <p className="center">{workingAddress}</p>
             <div className="flex space-x-2 center place-content-center p-2">
                 <button className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" onClick={setAddress}> Set Address</button>
                 <button className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" onClick={setPort}>Set Port</button>
+                <button className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" onClick={connect}>Connect</button>
             </div>
             <div ref={touchPadRef} className="border-solid border-2 border-sky-500 w-full h-96" onTouchMove={onMove} onTouchStart={onStart} onTouchEnd={onEnd} onMouseDown={onStart} onMouseUp={onEnd} onMouseMove={onMove}>
             </div >
